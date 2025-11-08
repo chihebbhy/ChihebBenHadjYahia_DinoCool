3bcb864179321c0ad7613d562cef06a396a140f6
chihebbhy <chiheb147bhy@gmail.com>
2025-11-08 02:08:03 +0200

    Added moving floor and changed alot of the infrastructure

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+// Bugs to fix : 
+// when restarting GameLoop keeps running and player falls
+// Animation t3 kolchy
+
+// Features to add :
+// Biomes.
+// More speed over time
+
+
 // DOMs 
 const playerDOM = document.getElementById("Player");
 const floorDOM = document.getElementById("Floor");
@@ -7,13 +16,87 @@ const gameDOM = document.getElementById("Game");
 
 // Constants 
 const g = -1000;    //gravity 
+const BiomeImage = {
+    grass: 'Resources/GreenGrassBlock.png',
+    snow: 'Resources/snowy.png'
+}
+let Floors = [];
+let GameLoopId;
+
 
 //fps related variables 
 let GameStarted = false;
 let fpslimit = 144;
 let interval = 1000 / fpslimit;
-let lastFrameTimeMs = 0;
+let LastFrameTime = 0;
 let dt = 0;
+
+// our entity class
+class Entity {
+    constructor(EntityId, Name, Image, Velocity) {
+        this.EntityDOM = this.CreateEntity(EntityId, Image);
+        this.Name = Name;
+        this.PositionBottom = parseInt(window.getComputedStyle(this.EntityDOM).bottom);
+        this.PositionTop = parseInt(window.getComputedStyle(this.EntityDOM).bottom) + parseInt(window.getComputedStyle(this.EntityDOM).height);
+        this.PositionLeft = parseInt(window.getComputedStyle(this.EntityDOM).left);
+        this.PositionRight = parseInt(window.getComputedStyle(this.EntityDOM).left) + parseInt(window.getComputedStyle(this.EntityDOM).width);
+        this.Velocity = Velocity;
+
+    }
+    CreateEntity(EntityId, Image) {
+        var entity = document.createElement("div");
+        entity.setAttribute("id", EntityId);
+        entity.style.backgroundImage = `url("${Image}")`;
+        if (EntityId.includes("Floor")) {
+            document.getElementById("Floor").appendChild(entity);
+        } else {
+            document.getElementById("Enemies").appendChild(entity);
+        }
+        return entity;
+    }
+    Deconstruct() {
+        this.EntityDOM.parentNode.removeChild(this.EntityDOM);
+        this.Name = null;
+        this.PositionBottom = null;
+        this.PositionLeft = null;
+        this.PositionRight = null;
+        this.PositionTop = null;
+        this.Velocity = null;
+
+    }
+}
+
+// our floor class
+class Floor extends Entity {
+    constructor(Name, Image, Velocity) {
+        super(Name, Name, Image, Velocity);
+    }
+    reload() {              // to fix the zoom bug
+        const css = window.getComputedStyle(this.EntityDOM);
+        this.PositionBottom = parseInt(css.bottom);
+        this.PositionTop = parseInt(css.bottom) + parseInt(css.height);
+        this.PositionLeft = parseInt(css.left);
+        this.PositionRight = parseInt(css.left) + parseInt(css.width);
+    }
+    ChangeImage(newImage) { // for different biomes
+        this.EntityDOM.style.backgroundImage = `  url("${newImage}")  `;
+    }
+    ExpandFloor() {                 // for the animation when starting the game
+        this.EntityDOM.classList.add("FloorExpanded");
+        setTimeout(() => {          // tna7i el class ejdid ama trod el width 200vw  for cleaner css
+            this.EntityDOM.classList.remove("FloorExpanded");
+            this.EntityDOM.style.width = "200vw";
+            this.EntityDOM.style.transition = "";
+        }, 1000);
+    }
+    MoveFloor(seconds){
+        this.PositionLeft -= this.Velocity*seconds;
+        this.PositionRight -= this.PositionLeft +  this.EntityDOM.offsetWidth;
+        this.EntityDOM.style.left = this.PositionLeft + "px"
+        
+    }
+
+};
 
 // our player
 let Player = {
@@ -32,56 +115,25 @@ let Player = {
         this.PositionLeft = parseInt(PlayerCSS.left);
         this.PositionRight = parseInt(PlayerCSS.left) + parseInt(PlayerCSS.width);
 
+    },
+    ApplyPhysics(seconds){
+        this.Velocity += g * seconds;
+        this.PositionBottom += Player.Velocity * seconds;
+    },
+    AddScore(toAdd){
+        this.Score += toAdd ;
     }
 };
 
-// our floor
-let Floor = {
-    PositionBottom: parseInt(window.getComputedStyle(floorDOM).bottom), // initial position in pixels(computed from css) 
-    PositionTop: parseInt(window.getComputedStyle(floorDOM).bottom) + parseInt(window.getComputedStyle(floorDOM).height),
-    PositionLeft: -Infinity,
-    PositionRight: Infinity,
-    reload() {
-        var FloorCSS = window.getComputedStyle(floorDOM);
-        this.PositionBottom = parseInt(FloorCSS.bottom);
-        this.PositionTop = parseInt(FloorCSS.bottom) + parseInt(FloorCSS.height);
-    }
-};
-
-// zooming or resizing window (khatr lzoom kent tbuggi)
-window.addEventListener('resize', function (event) {
-    Floor.reload();
-    Player.reload();
-    if(Player.OnGround){
-        Player.PositionBottom = Floor.PositionTop;
-    }
-})
-
-// to make zooming in and out and resizing tabs work properly 
-document.addEventListener('keydown', function (event) {
-    console.log(Player.OnGround);
-    if (GameStarted) {
-        var key = event.key;
-        var code = event.code;
-        if (code == "Space") {
-            // ynagz* 
-            if (Player.OnGround == true) {
-                Player.Velocity = 650; // valeur 3ejbetni
-                Player.OnGround = false;
-                playerDOM.classList.add("JumpAndRotate");
-            }
-        }
-    }
-}
-)
-
-function isColliding(a,b){
-    return !( a.PositionRight < b.PositionLeft ||
-        a.PositionLeft > b.PositionRight  ||
-        a.PositionTop < b.PositionBottom  ||
-        a.PositionBottom > b.PositionTop  
+// test if a and b collide based on them .Position
+function isColliding(a, b) {
+    return !(a.PositionRight < b.PositionLeft ||
+        a.PositionLeft > b.PositionRight ||
+        a.PositionTop < b.PositionBottom ||
+        a.PositionBottom > b.PositionTop
     );
 }
+// hides menu and shows game
 function HideMenuAndShowGame(ok) {
     if (ok) {
         menuDOM.style.visibility = "hidden";
@@ -91,21 +143,27 @@ function HideMenuAndShowGame(ok) {
         gameDOM.style.visibility = "hidden";
     }
 }
+
+
+
+// runs each frame
 function UpdateGame(dt) {
     // calcul physique
     const seconds = dt / 1000;
-    Player.Velocity += g * seconds;
-    Player.PositionBottom += Player.Velocity * seconds;
-
-    // calcul score
-    Player.Score += seconds * 10; // 10 score par seconde
-
-    if (Player.PositionBottom <= Floor.PositionTop) {          // collision detection
-        Player.PositionBottom = Floor.PositionTop;
-        Player.Velocity = 0;
-        if (!Player.OnGround) {
-            Player.OnGround = true;
-            playerDOM.classList.remove("JumpAndRotate");
+    for (const f of Floors){
+        f.MoveFloor(seconds);
+        f.reload();
+    }
+    Player.ApplyPhysics(seconds);
+    Player.AddScore(seconds*10); // 10 score par seconde
+    for (const f of Floors) {
+        if (isColliding(Player, f)) {          // collision detection
+            Player.PositionBottom = f.PositionTop;
+            Player.Velocity = 0;
+            if (!Player.OnGround) {
+                Player.OnGround = true;
+                playerDOM.classList.remove("JumpAndRotate");
+            }
         }
     }
     playerDOM.style.bottom = Player.PositionBottom + "px";    // For player jumping 
@@ -115,12 +173,12 @@ function UpdateGame(dt) {
 
 // loop tcontrolli lframes bch mayfoutouch 144 fps w maydhay3ouch frames 
 function GameLoop(TimeStamp) {
-    if (TimeStamp < (lastFrameTimeMs + interval)) {
+    if (TimeStamp < (LastFrameTime + interval)) {
         requestAnimationFrame(GameLoop);
         return;
     }
-    dt += TimeStamp - lastFrameTimeMs;
-    lastFrameTimeMs = TimeStamp;
+    dt += TimeStamp - LastFrameTime;
+    LastFrameTime = TimeStamp;
     while (dt >= interval) {
         // trajja3 eli je retard 
         UpdateGame(interval);
@@ -130,17 +188,97 @@ function GameLoop(TimeStamp) {
 }
 
 
+
 function StartGame() {
+    const btn = document.getElementById("StartButton");
+    btn.style.display = "none";
+    btn.disabled = true;
+    btn.blur();
+    const btn2 = document.getElementById("ResetGame");
+    btn2.style.display = "block";
+    btn2.disabled = false;  
     HideMenuAndShowGame(true);
-    document.getElementById("StartButton").disabled = true;
+    Player.Score = 0;
+    Player.Velocity = 0;
+    Player.Ducking = false;
+    Player.OnGround = true;
     GameStarted = true;
-    lastFrameTimeMs = performance.now();
-    GameLoop(0);
-    floorDOM.classList.add("FloorExpanded");
-    setTimeout(() => {      // tna7i el class ejdid ama trod el width 200vw  for cleaner css
-        floorDOM.classList.remove("FloorExpanded");
-        floorDOM.style.width = "200vw";
-        floorDOM.style.transition = "none";
-    }, 1000);
-    console.log(Player);
+
+    //Floor management-----------
+    Floors = [
+        new Floor("Floor1", BiomeImage.grass, 200),
+    ];
+    Floors[0].ExpandFloor();
+    console.log(Floors);
+    //---------------------------
+    LastFrameTime = performance.now();
+    GameLoopId = GameLoop(0);
+
 }
+
+// Restart 
+function ResetGame() {
+    //for (const enemy of Enemies) {enemy.Deconstruct()};
+    //Enemies = [];
+    for (const floor of Floors) { floor.Deconstruct(); }
+    Floors = [];
+    HideMenuAndShowGame(false);
+    const btn = document.getElementById("StartButton");
+    btn.style.display = "block";
+    btn.disabled = false;
+    
+    document.getElementById("ResetGame").style.display = "none";
+    document.getElementById("ResetGame").disabled = true;
+    document.getElementById("ResetGame").blur();
+
+    Player.Score = 0;
+    Player.Velocity = 0;
+    Player.PositionBottom = 0;
+
+    GameStarted = false;
+}
+
+
+
+// zooming or resizing window (khatr lzoom kent tbuggi)
+window.addEventListener('resize', function (event) {
+    for (const f of Floors) {
+        f.reload();
+    }
+    Player.reload();
+    if (Player.OnGround && Floors.length > 0) {
+        Player.PositionBottom = Floors[0].PositionTop;
+    }
+
+})
+
+// to jump or duck
+document.addEventListener('keydown', function (event) {
+    if (GameStarted) {
+        var key = event.key;
+        var code = event.code;
+        console.log(playerDOM.classList);
+        if (code == "Space") {
+            // ynagz* 
+            if (Player.OnGround == true) {
+                Player.Velocity = 650; // valeur 3ejbetni
+                Player.OnGround = false;
+                playerDOM.classList.add("JumpAndRotate");
+            }
+        }else{
+            if(code == "ArrowDown"){
+                //duck
+                if(Player.OnGround == true){
+                    playerDOM.classList.add("Duck");
+                    playerDOM.classList.remove("Standing");
+                }
+            }else{
+                if(playerDOM.classList.contains("Duck")){
+                    playerDOM.classList.remove("Duck");
+                    playerDOM.classList.add("Standing");
+                }
+            }
+        }
+    }
+}
+)
